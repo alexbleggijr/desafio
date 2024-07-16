15657b617cdacc63dbde0433408437a780069c5c
Hallef <hallefcoelho@gmail.com>
2024-07-16 18:31:21 -0300

    Ajustes no mix

--- src/components/ds-card/ds-card.stories.ts
+++ src/components/ds-card/ds-card.stories.ts
@@ -1,12 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 
+function generateColorOptions(name: string) {
+  const nameList = [];
+  for (let i = 1; i <= 5; i++) {
+    nameList.push(`ds-color-${name}-${i}`);
+  }
+  return nameList;
+}
+
 const meta: Meta<any> = {
   title: 'Card',
   argTypes: {
     color: {
       control: 'select',
-      options: ['color'],
+      options: generateColorOptions('brand'),
+      description: 'Define a Cor.',
+
     },
   },
 };
@@ -17,10 +27,11 @@ type Story = StoryObj<any>;
 
 export const Default: Story = {
   render: ({ ...args }) => {
+    console.log(args)
     return html`
-      <ds-card color="${args.color}">
-        <ds-text>Lorem, ipsum dolor.</ds-text>
-      </ds-card>
+    <ds-card color="${args.color}">
+    <ds-text>Lorem, ipsum dolor.</ds-text>
+    </ds-card>
     `;
   },
 };
