f0a8bccb2e70f42629b15c83fbbca4508355fbd0
Hallef <hallefcoelho@gmail.com>
2024-07-24 20:29:58 -0300

    Definição do card

--- src/components/ds-card/ds-card.stories.ts
+++ src/components/ds-card/ds-card.stories.ts
@@ -53,7 +53,8 @@ export const Default: Story = {
 
     return html`
       <ds-card>
-        <ds-text>Lorem, ipsum dolor.</ds-text>
+        <h1>Lorem Ipsum</h1>
+        <ds-text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</ds-text>
       </ds-card>
     `;
   },
