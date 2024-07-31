2fa8ca039507f8ebddc491c48b93f8856eace3cc
Hallef <hallefcoelho@gmail.com>
2024-07-31 18:34:52 -0300

    Implementação dos neutrals alternative

--- src/components/utils/enums/color.enum.ts
+++ src/components/utils/enums/color.enum.ts
@@ -8,14 +8,23 @@ export enum DsColorBrand {
 
 export enum DsColorNeutral {
   Neutral_1 = 'neutral-1',
+  Neutral_15 = 'neutral-15',
   Neutral_2 = 'neutral-2',
+  Neutral_25 = 'neutral-25',
   Neutral_3 = 'neutral-3',
+  Neutral_35 = 'neutral-35',
   Neutral_4 = 'neutral-4',
+  Neutral_45 = 'neutral-45',
   Neutral_5 = 'neutral-5',
+  Neutral_55 = 'neutral-55',
   Neutral_6 = 'neutral-6',
+  Neutral_65 = 'neutral-65',
   Neutral_7 = 'neutral-7',
+  Neutral_75 = 'neutral-75',
   Neutral_8 = 'neutral-8',
+  Neutral_85 = 'neutral-85',
   Neutral_9 = 'neutral-9',
+  Neutral_95 = 'neutral-95',
   Neutral_10 = 'neutral-10',
 }
 
